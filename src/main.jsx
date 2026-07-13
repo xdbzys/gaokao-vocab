d6404c78069ca65f8038e21c4d2ed052c7ecfbfe
xdbzys <xdbzys@users.noreply.github.com>
2026-07-13 14:34:20 +0000

    v2.8.6: test update

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,8 @@ import './styles.css';
 /* ============================
    APP 版本常量
    ============================ */
-const APP_VERSION = '2.8.5';
-const APP_VERSION_CODE = 55;
+const APP_VERSION = '2.8.6';
+const APP_VERSION_CODE = 56;
 // 内置更新服务器地址（后续部署时修改此处即可，APP和网页版共用此地址）
 const GITEE_OWNER = 'xdbzys';
 const GITEE_REPO = 'app';
